cafba99b74cdc3313b008343c2b1765717c66926
Richard Garrison <anm8tr@users.noreply.github.com>
2022-10-08 15:30:39 -0700

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Richard Garrison on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
