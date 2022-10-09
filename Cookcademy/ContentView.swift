4a734f62f4c9c094ad1faaedd3f453b930b849cd
Richard Garrison <anm8tr@users.noreply.github.com>
2022-10-08 22:50:35 -0700

    Build recipe model

--- Cookcademy/ContentView.swift
+++ Cookcademy/ContentView.swift
@@ -7,20 +7,30 @@
 
 import SwiftUI
 
+let myIngredient = Ingredient(name: "Sugar", quantity: 2.0, unit: .tbs)
+
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            let name = myIngredient.description
+            Text("\(name)")
+            
+            
         }
         .padding()
     }
+
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
+
+
